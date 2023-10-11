376ee4bc6e3af2fa756b0a4ab5cb2872871191bc
turgayulutas <turgay@qbitcodes.com>
2023-10-11 15:41:10 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# rgis-vision-sample
-RGIS Vision Sample Application
+
+# RGIS Vision Sample Application
+
+The purpose of this sample app is to aid the RGIS team in adapting their current RGIS Counter app to properly launch the product label reader app Spyglass is building called "RGIS Vision". This sample application is designed for testing and understanding the RGIS Vision integration. The sample application receives the necessary parameters to invoke the RGIS Vision application and demonstrates how this integration is implemented.
+
+#### Responses and Errors
+- If the RGIS Vision application cannot be launched, an error message will be displayed.
+- If the integration is successful, the results and necessary operations will occur.
+
+#### Warnings
+- **IMPORTANT**: This version of RGIS Vision is not complete and is distributed strictly for integration purposes. Please do NOT show or share beyond the needs of the development team.
+- This application is created for testing and understanding the RGIS Vision integration. Do not use real databases or sensitive data.
+- This sample application can be used to test the RGIS Vision integration. It will help you understand how to provide parameters and observe the results.
+
+#### Notes
+- The sample application relies on the presence of the RGIS Vision application. Before using the sample application, please ensure that the RGIS Vision application is installed and properly set up on your device.
+- Currently, the sample application only works with the sample database downloaded from [this link](https://drive.google.com/file/d/1RJbKIzAuQVpvN7NPymL7On54Aj-96C-E/view?usp=sharing).
+- For how to install and use on a device, see below section "Usage Scenario"
+
+#### Download Sample Database
+You can download the sample database from [this link](https://drive.google.com/file/d/1RJbKIzAuQVpvN7NPymL7On54Aj-96C-E/view?usp=sharing).
+
+## Launching RGIS Vision
+To launch the RGIS Vision application, you can use the `launchRgisVision()` function. Before calling this function, ensure that the following parameters are properly set:
+
+- `database_file_path`: The file path of the database for scanning.
+- `table_name`: The name of the target table for scanning.
+- `search_column`: The name of the column for performing the search.
+- `description_column`: The name of the column where descriptions are obtained.
+
+```kotlin
+private fun launchRgisVision() {
+    val intent = Intent(Intent.ACTION_VIEW)
+
+    intent.apply {
+        component = ComponentName(
+            getString(R.string.rgis_vision_app_package),
+            getString(R.string.rgis_vision_app_scanner_activity)
+        )
+
+        putExtra(getString(R.string.key_database_file_path), selectedDatabasePath)
+        putExtra(getString(R.string.key_table_name), selectedTable)
+        putExtra(getString(R.string.key_search_column), selectedSearchColumn)
+        putExtra(getString(R.string.key_description_column), selectedDescriptionColumn)
+    }
+
+    try {
+        rgisVisionAppLauncher.launch(intent)
+    } catch (e: ActivityNotFoundException) {
+        showSnackBar(getString(R.string.error_rgis_vision_app_not_found))
+    }
+}
+```
+
+### Handling RGIS Vision Responses
+After launching RGIS Vision, the response can be captured using the rgisVisionAppLauncher activity result launcher.
+
+If the scan is successful, the result will be returned with a JSON response under `Activity.RESULT_OK`. If an error occurs during the scan, it will be returned under `Activity.RESULT_CANCELED`.
+
+Make sure to handle both cases to properly respond to the RGIS Vision scan results.
+
+
+
+```kotlin
+    private val rgisVisionAppLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.let { intent ->
+                    intent.getBundleExtra(getString(R.string.key_rgis_vision_response))
+                        ?.let { scanResponse ->
+                            binding.tvScanResponseJson.text =
+                                scanResponse.getString(getString(R.string.key_rgis_vision_response_json))
+                        }
+                }
+            } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                result.data?.let { intent ->
+                    intent.getBundleExtra(getString(R.string.key_rgis_vision_exception))
+                        ?.let { exceptionBundle ->
+                            showSnackBar(
+                                exceptionBundle.getString(getString(R.string.key_rgis_vision_exception))
+                                    ?: "Unknown Error"
+                            )
+                        }
+                }
+            }
+        }
+```
+
+### RGIS Vision Sample Application Usage Scenario
+Below is an example scenario for using the RGIS Vision Sample application:
+
+##### To install on a device (one time):
+1. First download the "RGIS Vision" application provided to you > Open the app and allow both required permissions.
+2. Then download the "RGIS Vision Sample" application provided to you.
+3. Download the sample database from this link to your device.
+##### To use and test after install:
+1. In the Sample app load the downloaded database using the Select Database button.
+2. Select the `products` table.
+3. Choose `product_id` as the `Search Column`.
+4. Select `product_name` or `product_description` as the `Description Column`.
+5. Click the "Launch RGIS Vision" button to start the RGIS Vision application.
+
+Once the RGIS Vision application is launched, you can scan the following example "product_id"s to observe the results:
+
+- 10890EB0
+
+- 138585D0
+
+These two `product_id` will return a single result.
+
+Additionally, to try a scenario where multiple results are found, scan the following "product_id":
+
+- 2C5E2623
+
+
+Note: To ensure accurate scanning, make sure that the scanning frame contains only the text to be scanned. Please ensure that there are no extra objects outside the frame.
---
+++ app/src/main/java/com/spyglass/rgisvisionsample/MainActivity.kt
@@ -0,0 +1,252 @@
+package com.spyglass.rgisvisionsample
+
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.Settings
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.spyglass.rgisvisionsample.databinding.ActivityMainBinding
+import com.spyglass.rgisvisionsample.utils.FileUtils
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var selectedDatabasePath: String? = null
+    private var selectedTable: String? = null
+    private var selectedSearchColumn: String? = null
+    private var selectedDescriptionColumn: String? = null
+
+    private val selectDatabaseLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let {
+                    FileUtils(this@MainActivity).getPath(it)?.let { databasePath ->
+                        binding.tvSelectedDatabasePath.text = databasePath
+                        openDatabase(File(databasePath))
+                    }
+                }
+            }
+        }
+
+    private val rgisVisionAppLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.let { intent ->
+                    intent.getBundleExtra(getString(R.string.key_rgis_vision_response))
+                        ?.let { scanResponse ->
+                            binding.tvScanResponseJson.text =
+                                scanResponse.getString(getString(R.string.key_rgis_vision_response_json))
+                        }
+                }
+            } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                result.data?.let { intent ->
+                    intent.getBundleExtra(getString(R.string.key_rgis_vision_exception))
+                        ?.let { exceptionBundle ->
+                            showSnackBar(
+                                exceptionBundle.getString(getString(R.string.key_rgis_vision_exception))
+                                    ?: "Unknown Error"
+                            )
+                        }
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLaunchRgisVision.setOnClickListener {
+            launchRgisVision()
+        }
+
+        binding.btnSelectDatabase.setOnClickListener {
+            if (checkReadStoragePermission().not()) {
+                requestReadStoragePermission()
+            } else {
+                launchSelectDatabaseIntent()
+            }
+        }
+    }
+
+    private fun launchSelectDatabaseIntent() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            type = "*/*"
+            addCategory(Intent.CATEGORY_OPENABLE)
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, true)
+        }
+        selectDatabaseLauncher.launch(intent)
+    }
+
+    private fun openDatabase(databaseFile: File) {
+        runCatching {
+            val sqliteDatabase = SQLiteDatabase.openOrCreateDatabase(databaseFile, null)
+            val tableNames = mutableListOf<String>()
+            val cursor =
+                sqliteDatabase.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null)
+
+            while (cursor.moveToNext()) {
+                val tableName = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                if (tableName != "android_metadata" && tableName != "sqlite_sequence") {
+                    tableNames.add(tableName)
+                }
+            }
+
+            cursor.close()
+
+            val tableAdapter =
+                ArrayAdapter(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    tableNames
+                )
+
+            binding.dropdownDatabaseTable.setAdapter(tableAdapter)
+
+            binding.dropdownDatabaseTable.onItemClickListener =
+                AdapterView.OnItemClickListener { _, _, position, _ ->
+                    selectedTable = tableNames[position]
+
+                    val tableCursor: Cursor =
+                        sqliteDatabase.query(selectedTable, null, null, null, null, null, null)
+
+                    val columnsAdapter =
+                        ArrayAdapter(
+                            this,
+                            android.R.layout.simple_list_item_1,
+                            android.R.id.text1,
+                            tableCursor.columnNames
+                        )
+
+                    binding.dropdownSearchColumn.setAdapter(columnsAdapter)
+                    binding.dropdownDescriptionColumn.setAdapter(columnsAdapter)
+
+                    binding.dropdownSearchColumn.onItemClickListener =
+                        AdapterView.OnItemClickListener { _, _, searchPosition, _ ->
+                            selectedSearchColumn = tableCursor.columnNames[searchPosition]
+                        }
+
+                    binding.dropdownDescriptionColumn.onItemClickListener =
+                        AdapterView.OnItemClickListener { _, _, descriptionPosition, _ ->
+                            selectedDescriptionColumn = tableCursor.columnNames[descriptionPosition]
+                        }
+
+                    tableCursor.close()
+                }
+
+            selectedDatabasePath = databaseFile.path
+            //sqliteDatabase.close()
+        }.getOrElse {
+            it.printStackTrace()
+            MaterialAlertDialogBuilder(this)
+                .setTitle(getString(R.string.error_database_cannot_open))
+                .setMessage(it.message.toString())
+                .setNegativeButton(getString(android.R.string.ok)) { dialog, _ ->
+                    dialog.dismiss()
+                }.show()
+        }
+    }
+
+    private fun launchRgisVision() {
+        if (checkAllParamsIsDone().not())
+            return
+
+        val intent = Intent(Intent.ACTION_VIEW)
+
+        intent.apply {
+            component = ComponentName(
+                getString(R.string.rgis_vision_app_package),
+                getString(R.string.rgis_vision_app_scanner_activity)
+            )
+
+            putExtra(getString(R.string.key_database_file_path), selectedDatabasePath)
+            putExtra(getString(R.string.key_table_name), selectedTable)
+            putExtra(getString(R.string.key_search_column), selectedSearchColumn)
+            putExtra(getString(R.string.key_description_column), selectedDescriptionColumn)
+        }
+
+        try {
+            rgisVisionAppLauncher.launch(intent)
+        } catch (e: ActivityNotFoundException) {
+            showSnackBar(getString(R.string.error_rgis_vision_app_not_found))
+        }
+    }
+
+    private fun checkAllParamsIsDone(): Boolean {
+        if (selectedDatabasePath == null) {
+            showSnackBar(getString(R.string.error_please_select_database))
+            return false
+        }
+
+        if (selectedTable == null) {
+            showSnackBar(getString(R.string.error_please_select_table))
+            return false
+        }
+
+        if (selectedSearchColumn == null) {
+            showSnackBar(getString(R.string.error_please_select_search_column))
+            return false
+        }
+
+        if (selectedDescriptionColumn == null) {
+            showSnackBar(getString(R.string.error_please_select_description_column))
+            return false
+        }
+
+        return true
+    }
+
+    private fun checkReadStoragePermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            val readStorage =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            val writeStorage =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            readStorage == PackageManager.PERMISSION_GRANTED &&
+                    writeStorage == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun requestReadStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            intent.data = Uri.parse("package:${packageName}")
+            startActivity(intent)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                11
+            )
+        }
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+}
